d557a91a0aed03f6cd4616bfbb2396ba7f840165
Vladislav6410 <vladoexport@gmail.com>
2026-01-15 05:15:26 +0100

    Create SecureStore.swift

---
+++ App/Utils/SecureStore.swift
@@ -0,0 +1,51 @@
+import Foundation
+import Security
+
+final class SecureStore {
+
+    static let shared = SecureStore()
+    private init() {}
+
+    func save(_ value: String, for key: String) {
+        let data = Data(value.utf8)
+
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key,
+            kSecValueData as String: data
+        ]
+
+        SecItemDelete(query as CFDictionary)
+        SecItemAdd(query as CFDictionary, nil)
+    }
+
+    func load(for key: String) -> String? {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key,
+            kSecReturnData as String: true,
+            kSecMatchLimit as String: kSecMatchLimitOne
+        ]
+
+        var item: AnyObject?
+        let status = SecItemCopyMatching(query as CFDictionary, &item)
+
+        guard
+            status == errSecSuccess,
+            let data = item as? Data
+        else {
+            return nil
+        }
+
+        return String(decoding: data, as: UTF8.self)
+    }
+
+    func delete(for key: String) {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key
+        ]
+
+        SecItemDelete(query as CFDictionary)
+    }
+}
