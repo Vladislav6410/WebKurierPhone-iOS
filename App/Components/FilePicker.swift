c022c80411aa1acde59252072547390aa19f872a
Vladislav6410 <vladoexport@gmail.com>
2026-01-18 14:37:11 +0100

    Create FilePicker.swift

---
+++ App/Components/FilePicker.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+import UniformTypeIdentifiers
+
+struct FilePicker: UIViewControllerRepresentable {
+
+    var allowedTypes: [UTType] = [.item]
+    var onPicked: (URL) -> Void
+
+    func makeUIViewController(context: Context) -> UIDocumentPickerViewController {
+        let picker = UIDocumentPickerViewController(forOpeningContentTypes: allowedTypes, asCopy: true)
+        picker.delegate = context.coordinator
+        picker.allowsMultipleSelection = false
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIDocumentPickerViewController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(onPicked: onPicked)
+    }
+
+    final class Coordinator: NSObject, UIDocumentPickerDelegate {
+
+        let onPicked: (URL) -> Void
+
+        init(onPicked: @escaping (URL) -> Void) {
+            self.onPicked = onPicked
+        }
+
+        func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+            guard let url = urls.first else { return }
+            onPicked(url)
+        }
+    }
+}
